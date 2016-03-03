245848f4690414582fa043dc50a27d68afe4e090
David Yoonsik Yoo <yu.david880@gmail.com>
2016-03-04 05:03:27 +0900

    D3.js programming added.

---
+++ D3_Programming/app/models/SubjectViews.js
@@ -0,0 +1,7 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+// define our nerd model
+// module.exports allows us to pass this to other files when it is called
+module.exports = connectionsubject.model('', {}, 'projects');
---
+++ D3_Programming/config/db.js
@@ -0,0 +1,7 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+module.exports = {
+	urlSubjectViews: 'mongodb://localhost:27017/donorschoose', // More path can be added to the mongodb address
+}
